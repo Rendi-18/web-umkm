2a2064ed896a155c78b61b55fbc0500eea123fb8
Rendi-18 <rendidwipratama99@gmail.com>
2022-06-16 00:02:30 +0700

    struktur

---
+++ resources/views/configs/css.blade.php
@@ -0,0 +1,7 @@
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
