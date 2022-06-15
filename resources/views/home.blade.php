9e552f5d8c36efe1f0250ce6a2fb2785f99ac8ff
Tomi Andrean <axtomy9@gmail.com>
2022-06-16 02:51:16 +0700

    xxx

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
                         {{ __('You are logged in!') }}
 
-                        {{-- xxx --}}
+                        {{-- yyy --}}
                     </div>
                 </div>
             </div>
