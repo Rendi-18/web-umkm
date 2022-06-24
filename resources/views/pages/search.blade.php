db54c14a8576d2f10a60c2111976195d3614a8a8
Rendi-18 <rendidwipratama99@gmail.com>
2022-06-24 09:38:06 +0700

    page

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -50,7 +50,7 @@
             </div>
         </section><!-- End Portfolio Section -->
         <div class="d-flex justify-content-center pt-3">
-            {{ $umkms->links('Components.Paginator') }}
+            {{ $umkms->links('Components.paginator') }}
         </div>
         </section><!-- End Breadcrumbs -->
 
