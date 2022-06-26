58cf292b4dc1762e8f9895b89b9d6f34ec774783
Rendi-18 <rendidwipratama99@gmail.com>
2022-06-26 22:44:13 +0700

    dashboard

---
+++ resources/views/layouts/dashboard.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    @include('configs.css')
+
+</head>
+
+<body>
+    <div id="app">
+        @yield('content')
+    </div>
+    <div id="preloader"></div>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center">
+        <i class="bi bi-arrow-up-short"></i>
+    </a>
+
+    @include('configs.js')
+
+</body>
+
+</html>
