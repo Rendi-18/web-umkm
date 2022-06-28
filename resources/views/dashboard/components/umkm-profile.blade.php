ec69a7fb022a2a1e3c6a17fa7fe98f248f36719a
Tomi Andrean <axtomy9@gmail.com>
2022-06-29 03:13:58 +0700

    dashboard-form-card-product

--- resources/views/dashboard/components/umkm-profile.blade.php
+++ resources/views/dashboard/components/umkm-profile.blade.php
@@ -1,36 +1,145 @@
-<div class="row g-4 mb-5">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="row">
-                <div class="col-6 py-5 px-5 pl-5">
-                    <img src="/img/elements/2.jpg" class=" h-auto rounded-circle img-fluid">
-                </div>
-                <div class="col-6 py-5 px-5 pl-5">
-                    <h3>nama umkm</h3>
-                    <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, aliquam. Maiores alias veniam
-                        distinctio itaque! Dolor aspernatur quos ducimus consequatur ab distinctio. A maxime
-                        necessitatibus obcaecati? Alias odit adipisci nam?</h6>
-                    <button type="button" class="btn rounded-pill btn-primary">
-                        <span class="tf-icons bx bx-pie-chart-alt"></span>&nbsp; Primary
-                    </button>
-                </div>
-            </div>
+<section id="prfile">
+    <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">UMKM/</span> Edit profile</h4>
+    <div class="row g-4 mb-5">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="row">
+                    <div class="col-4 py-5 px-5 pl-5">
+                        <img src="/img/elements/2.jpg" class="img-pr h-auto rounded-circle img-fluid">
+                    </div>
+                    <div class="col-8 py-5 px-5 pl-5">
+                        <h3>nama umkm</h3>
+                        <h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, aliquam. Maiores alias
+                            veniam
+                            distinctio itaque! Dolor aspernatur quos ducimus consequatur ab distinctio. A maxime
+                            necessitatibus obcaecati? Alias odit adipisci nam?</h6>
+                        <div class="row">
+                            <div class="col-4">
+                                <h6><i class="bx bx-phone"></i> : 085669920357</h6>
+                            </div>
+                            <div class="col-8">
+                                <h6><i class="bx bx-map-alt"></i> : Lorem ipsum dolor sit, amet consectetur </h6>
+                            </div>
+                        </div>
+                        <button type="button" class="btn rounded-pill btn-primary">
+                            <span class="tf-icons bx bx-edit"></span>&nbsp; Edit
+                        </button>
 
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
-</div>
-<div class="row g-4 mb-5">
-    <div class="col-3">
-        <div class="card h-100">
-            <img class="card-img-top" src="/img/elements/2.jpg" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">nama Product</h5>
-                <p class="card-text">Total jumlah</p>
-                <button type="button" class="btn btn-outline-primary">
-                    <span class="tf-icons bx bx-pie-chart-alt"></span>&nbsp; Terlaris
+    <div class="row g-4 mb-5">
+        <div class="row">
+            <div class="col-6">
+                <h5 class="fw-bold">Produk Unggulan</h5>
+            </div>
+            <div class="col-6 d-flex ">
+                <button type="button" class="btn btn-primary ms-auto">
+                    <span class="tf-icons bx bx-plus"></span>&nbsp; Tambah Produk Unggulan
                 </button>
             </div>
         </div>
+        <div class="col-3">
+            <div class="card h-100">
+                <img class="card-img-top" src="/img/elements/2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">nama Product</h5>
+                    <p class="card-text">Total jumlah</p>
+                    <input type="checkbox" class="btn-check" id="btncheck2" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btncheck2"><i class='bx bxl-product-hunt'></i>
+                        Unggulan</label>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="form-edit"class="d-none">
+    <h4 class="col-6 fw-bold py-3 mb-4"><span class="text-muted fw-light">From/</span> Edit profile</h4>
+
+
+
+    <div class="col-xxl">
+        <div class="card mb-4">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <h5 class="mb-0">Edit Profile</h5> <small class="text-muted float-end">Pastikan data yang anda
+                    masukkan benar</small>
+            </div>
+            <div class="card-body">
+                <form>
+                    <div class="col-3 py-3 px-3 pl-5">
+                        <img src="/img/elements/2.jpg" class="img-pr h-auto rounded-circle img-fluid">
+                    </div>
+                    <div class="row mb-3">
+
+
+                        <label class="col-sm-2 col-form-label" for="inputGroupFile02">Foto Profile</label>
+                        <div class="col-sm-10">
+                            <div class="input-group">
+                                <input type="file" class="form-control" id="inputGroupFile02">
+                                <label class="input-group-text" for="inputGroupFile02">Upload</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 col-form-label" for="basic-icon-default-fullname">Name</label>
+                        <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                                <span id="basic-icon-default-fullname2" class="input-group-text"><i
+                                        class="bx bx-store-alt"></i></span>
+                                <input type="text" class="form-control" id="basic-icon-default-fullname"
+                                    placeholder="Name" aria-label="John Doe"
+                                    aria-describedby="basic-icon-default-fullname2">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label class="col-sm-2 form-label" for="basic-icon-default-phone">Phone No</label>
+                        <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                                <span id="basic-icon-default-phone2" class="input-group-text"><i
+                                        class="bx bx-phone"></i></span>
+                                <input type="text" id="basic-icon-default-phone" class="form-control phone-mask"
+                                    placeholder="+62" aria-label="658 799 8941"
+                                    aria-describedby="basic-icon-default-phone2">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 form-label" for="basic-icon-default-phone">Address</label>
+                        <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                                <span id="basic-icon-default-address" class="input-group-text"><i
+                                        class="bx bx-map-alt"></i></span>
+                                <input type="text" id="basic-icon-default-phone" class="form-control phone-mask"
+                                    placeholder="Address" aria-label="658 799 8941"
+                                    aria-describedby="basic-icon-default-address">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <label class="col-sm-2 form-label" for="basic-icon-default-message">Deskripsi</label>
+                        <div class="col-sm-10">
+                            <div class="input-group input-group-merge">
+                                <span id="basic-icon-default-message2" class="input-group-text"><i
+                                        class="bx bx-comment"></i></span>
+                                <textarea id="basic-icon-default-message" class="form-control" placeholder="Description..."
+                                    aria-label="Hi, Do you have a moment to talk Joe?" aria-describedby="basic-icon-default-message2"></textarea>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Send</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 
-</div>
+</section>
