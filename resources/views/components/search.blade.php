ed745f206645a8ca5f8de9487022e85939eb7ebf
Tomi Andrean <axtomy9@gmail.com>
2022-07-14 16:56:35 +0700

    formsearch

--- resources/views/components/search.blade.php
+++ resources/views/components/search.blade.php
@@ -6,55 +6,159 @@
         </div>
         <div class="row-content">
             <div class="row justify-content-center">
-                <div class="col-lg-8">
+                <div class="col-lg-8 row">
                     <p>Temukan UMKM & Unit Koperasi Di sekitar anda</p>
-
-                    <ul class="list-group">
+                    <div id="input-g"class="input-group">
                         @if (Request::is('search/umkm*'))
-                            <li class="list-group-item active" id="umkmSearch">UMKM</li>
-                            <li class="list-group-item" id="koperasiSearch">Koperasi</li>
+                            <form action="/search/umkm" method="get" id="formSearch" class="row p-0 input-group">
+                                <select class="form-select input-no-border col-3 shad-none"
+                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                    value="{{ request('search') }}" id="search">
+                                    <option>Default select</option>
+                                    @if (Request::is('search/umkm*'))
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @elseif (Request::is('search/koperasi*'))
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">
+                                            UMKM</option>
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">Koperasi</option>
+                                    @else
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @endif
+                                </select>
+                                <input type="text" class="col-6 form-control shad-none">
+                                <button class="col-3 form-control shad-none" type="submit">
+                                    <i class="bx bx-search"></i>
+                                </button>
+                            </form>
                         @elseif (Request::is('search/koperasi*'))
-                            <li class="list-group-item" id="umkmSearch">UMKM</li>
-                            <li class="list-group-item active" id="koperasiSearch">Koperasi</li>
+                            <form action="/search/koperasi" method="get" id="formSearch" class="row p-0 input-group">
+                                <select class="form-select input-no-border col-3 shad-none"
+                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                    value="{{ request('search') }}" id="search">
+                                    <option>Default select</option>
+                                    @if (Request::is('search/umkm*'))
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @elseif (Request::is('search/koperasi*'))
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">
+                                            UMKM</option>
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">Koperasi</option>
+                                    @else
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @endif
+                                </select>
+                                <input type="text" class="col-6 form-control shad-none">
+                                <button class="col-3 form-control shad-none" type="submit">
+                                    <i class="bx bx-search"></i>
+                                </button>
+                            </form>
                         @else
-                            <li class="list-group-item active" id="umkmSearch">UMKM</li>
-                            <li class="list-group-item" id="koperasiSearch">Koperasi</li>
+                            <form action="/search/umkm" method="get" id="formSearch" class="row p-0 input-group">
+                                <select class="form-select input-no-border col-3 shad-none"
+                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                    value="{{ request('search') }}" id="search">
+                                    <option>Default select</option>
+                                    @if (Request::is('search/umkm*'))
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @elseif (Request::is('search/koperasi*'))
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">
+                                            UMKM</option>
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">Koperasi</option>
+                                    @else
+                                        <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                            id="umkmSearch">UMKM</option>
+                                        <option class="list-group-item dropdown-item d-flex align-items-center"
+                                            id="koperasiSearch">
+                                            Koperasi</option>
+                                    @endif
+                                </select>
+                                <input type="text" class="col-6 form-control shad-none">
+                                <button class="col-3 form-control shad-none" type="submit">
+                                    <i class="bx bx-search"></i>
+                                </button>
+                            </form>
                         @endif
+                    </div>
 
-                    </ul>
-
-
-                    @if (Request::is('search/umkm*'))
-                        <form action="/search/umkm" method="get" id="formSearch" class="p-0">
-                            <div class="input-group justify-content-between">
-                                <input class="input-no-border m-2" type="text"
-                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
-                                    value="{{ request('search') }}" id="search">
-                                <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
-                                    id="button-addon2"><i class="bx bx-search"></i></button>
-                            </div>
-                        </form>
-                    @elseif (Request::is('search/koperasi*'))
-                        <form action="/search/koperasi" method="get" id="formSearch" class="p-0">
-                            <div class="input-group justify-content-between">
-                                <input class="input-no-border m-2" type="text"
-                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
-                                    value="{{ request('search') }}" id="search">
-                                <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
-                                    id="button-addon2"><i class="bx bx-search"></i></button>
-                            </div>
-                        </form>
-                    @else
-                        <form action="/search/umkm" method="get" id="formSearch" class="p-0">
-                            <div class="input-group justify-content-between">
-                                <input class="input-no-border m-2" type="text"
-                                    placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
-                                    value="{{ request('search') }}" id="search">
-                                <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
-                                    id="button-addon2"><i class="bx bx-search"></i></button>
-                            </div>
-                        </form>
-                    @endif
+                    {{-- <div class="col-lg-9 input-group justify-content-between">
+                        <select id="defaultSelect" class="form-select input-no-border">
+                            <option>Default select</option>
+                            @if (Request::is('search/umkm*'))
+                                <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                    id="umkmSearch">UMKM</option>
+                                <option class="list-group-item dropdown-item d-flex align-items-center"
+                                    id="koperasiSearch">
+                                    Koperasi</option>
+                            @elseif (Request::is('search/koperasi*'))
+                                <option class="list-group-item dropdown-item d-flex align-items-center" id="umkmSearch">
+                                    UMKM</option>
+                                <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                    id="koperasiSearch">Koperasi</option>
+                            @else
+                                <option class="list-group-item active dropdown-item d-flex align-items-center"
+                                    id="umkmSearch">UMKM</option>
+                                <option class="list-group-item dropdown-item d-flex align-items-center"
+                                    id="koperasiSearch">
+                                    Koperasi</option>
+                            @endif
+                        </select>
+                        @if (Request::is('search/umkm*'))
+                            <form action="/search/umkm" method="get" id="formSearch" class="p-0">
+                                <div class="input-group justify-content-between">
+                                    <input class="input-no-border m-2" type="text"
+                                        placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                        value="{{ request('search') }}" id="search">
+                                    <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
+                                        id="button-addon2"><i class="bx bx-search"></i></button>
+                                </div>
+                            </form>
+                        @elseif (Request::is('search/koperasi*'))
+                            <form action="/search/koperasi" method="get" id="formSearch" class="p-0">
+                                <div class="input-group justify-content-between">
+                                    <input class="input-no-border m-2" type="text"
+                                        placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                        value="{{ request('search') }}" id="search">
+                                    <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
+                                        id="button-addon2"><i class="bx bx-search"></i></button>
+                                </div>
+                            </form>
+                        @else
+                            <form action="/search/umkm" method="get" id="formSearch" class="p-0">
+                                <div class="input-group justify-content-between">
+                                    <input class="input-no-border m-2" type="text"
+                                        placeholder="Masukkan Nama UMKM/Unit Koperasi" name="search"
+                                        value="{{ request('search') }}" id="search">
+                                    <button class="btn btn-primary rounded-pill shadow-sm" type="submit"
+                                        id="button-addon2"><i class="bx bx-search"></i></button>
+                                </div>
+                            </form>
+                        @endif
+                    </div> --}}
                 </div>
             </div>
         </div>
--- resources/views/configs/js.blade.php
+++ resources/views/configs/js.blade.php
@@ -8,6 +8,7 @@
     <script src="/vendor/aos/aos.js"></script>
     <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
     <script src="/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="/vendor/js/bootstrap.js"></script>
     <script src="/vendor/isotope-layout/isotope.pkgd.min.js"></script>
     <script src="/vendor/swiper/swiper-bundle.min.js"></script>
     <script src="/vendor/waypoints/noframework.waypoints.js"></script>
