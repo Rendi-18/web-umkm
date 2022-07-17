32f8dc4ff999dd5ea841b67872146dbb236b3566
Tomi Andrean <axtomy9@gmail.com>
2022-07-17 11:07:25 +0700

    agenda

--- resources/views/pages/agenda/detail.blade.php
+++ resources/views/pages/agenda/detail.blade.php
@@ -1,4 +1,25 @@
 @extends('layouts.app')
 @section('content')
-    <h1 class="mt-5 pt-5">{{ $agenda->name }}</h1>
+    <main id="main" class="h-100">
+        <section id="breadcrumbs" class="breadcrumbs">
+            <div class="container h-100">
+                <ol>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/">UMKM</a></li>
+                    <li>Produk</li>
+                </ol>
+                <h2>{{ $agenda->name }}</h2>
+            </div>
+        </section>
+        <section id="dagendas">
+            <div class="container">
+                <div class="row">
+                    <div>
+                        <img src="img\portfolio\portfolio-3.jpg" class="img-fluid">
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </main>
 @endsection
--- resources/views/pages/agenda/index.blade.php
+++ resources/views/pages/agenda/index.blade.php
@@ -37,33 +37,36 @@
                         </div>
                     </section>
                 </div>
-                <div id="card-um" class="card-um">
-                    <div class="row">
-                        xxx
-                        @foreach ($agendas as $agenda)
-                            <div class="col-lg-6 my-4">
-                                <div class="member d-flex align-items-start" data-aos="zoom-in" data-aos-delay="100">
-                                    <div class="pic rounded">
-                                        @if ($agenda->image)
-                                            <img src="{{ asset('storage/' . $agenda->image) }}" class="img-fluid"
-                                                alt="">
-                                        @else
-                                            <img src="img\portfolio\portfolio-9.jpg" class="img-fluid" alt="">
-                                        @endif
-                                    </div>
-                                    <div class="member-info col-lg">
-                                        <h4>{{ $agenda->name }}</h4>
-                                        <small>{{ date('d F Y', strtotime($agenda->date)) }}</small>
-                                        <span>{{ $agenda->location }}</span>
-                                        <span class="text-justify">{!! Str::limit($agenda->content, 110) !!}</span>
-                                    </div>
-                                </div>
-                            </div>
-                        @endforeach
+                <section id="employee" class="employee  py-0">
+                    <div class="container" data-aos="fade-up">
+                        <div class="row">
+                            @foreach ($agendas as $agenda)
+                                <div class="col-lg-6 my-2">
+                                    <a href="agenda/{{ $agenda->id }}" class="agenda-card">
+                                        <div class="member d-flex align-items-start" data-aos="zoom-in"
+                                            data-aos-delay="100">
+                                            <div class="pic rounded">
+                                                @if ($agenda->image)
+                                                    <img src="{{ asset('storage/' . $agenda->image) }}" class="img-fluid"
+                                                        alt="">
+                                                @else
+                                                    <img src="img\portfolio\portfolio-9.jpg" class="img-fluid"
+                                                        alt="">
+                                                @endif
+                                            </div>
+                                            <div class="member-info col-lg">
+                                                <h4>{{ $agenda->name }}</h4>
+                                                <small>{{ date('d F Y', strtotime($agenda->date)) }}</small>
+                                                <span>{{ $agenda->location }}</span>
+                                                {!! Str::limit($agenda->content, 110) !!}
+                                            </div>
+                                        </div>
 
+                                </div>
+                        </div></a>
+                        @endforeach
                     </div>
-                </div>
-            </div>
+                </section>
         </section>
     </main>
 @endsection
