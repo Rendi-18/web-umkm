a7b864448a0a2673cc7e69639d83e6eeccce2043
Rendi-18 <rendidwipratama99@gmail.com>
2022-07-27 16:23:29 +0700

    dana

---
+++ resources/views/dashboard/pages/dana/dana.blade.php
@@ -0,0 +1,233 @@
+<section id="regist-koperasi">
+    <h4 class="col-6 fw-bold pb-2 mb-2">Pengajuan Bantuan Dana</h4>
+
+    {{-- Flash Message --}}
+    @if (session()->has('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            {{ session('success') }}
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
+
+    <div class="row g-4 mb-3">
+        <div class="col-lg-12">
+            <div class="row">
+                <div class="col-lg">
+                    <div class="card">
+                        <h5 class="card-header">Form Pengajuan Bantuan Dana</h5>
+                        <div class="card-body">
+                            <form class="" action="/dashboard/bantuan/dana" method="POST"
+                                enctype="multipart/form-data">
+                                @csrf
+                                {{-- NIB/NIK --}}
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="nib">NIK/NIB</label>
+                                    <div class="col-sm-10">
+                                        <div class="input-group input-group-merge">
+                                            <span class="input-group-text "><i class="bx bx-barcode"></i></span>
+                                            <input type="text" name="nib"
+                                                class="form-control @error('nib') is-invalid @enderror" id="nib"
+                                                placeholder="Nomor Induk Koperasi" value="{{ old('nib') }}" required>
+                                            @error('name')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                {{-- Name --}}
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="name">Nama
+                                        UMKM</label>
+                                    <div class="col-sm-10">
+                                        <div class="input-group input-group-merge">
+                                            <span class="input-group-text "><i class="bx bx-store-alt"></i></span>
+                                            <input type="text" name="name"
+                                                class="form-control @error('name') is-invalid @enderror"" id="name"
+                                                placeholder="Nama" aria-describedby="basic-icon-default-fullname2"
+                                                value="{{ old('name') }}" required>
+                                            @error('name')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- Phonenumber --}}
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="phonenumber">Phone No</label>
+                                    <div class="col-sm-10">
+                                        <div class="input-group input-group-merge">
+                                            <span id="basic-icon-default-phone2" class="input-group-text"><i
+                                                    class="bx bx-phone"></i></span>
+                                            <input type="number" name="phonenumber" id="phonenumber"
+                                                class="form-control phone-mask @error('phonenumber') is-invalid @enderror"
+                                                placeholder="+62" aria-label="658 799 8941"
+                                                aria-describedby="basic-icon-default-phone2"
+                                                value="{{ old('phonenumber') }}" required>
+                                            @error('phonenumber')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- File --}}
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="image">Proposal</label>
+                                    <div class="col-sm-10 d-flex align-items-start align-items-sm-center gap-4 ">
+                                        <div class="img-container img-container-sm rounded">
+                                            <img src="/img/temp/product-temp.png" alt="user-avatar"
+                                                class="img-preview d-block img-fluid img-fit mx-auto d-block"
+                                                id="uploadedAvatar">
+                                        </div>
+
+                                        <div class="button-wrapper">
+                                            <label for="upload" class="btn btn-primary me-2 mb-4" tabindex="0">
+                                                <span class="d-none d-sm-block">Upload Proposal</span>
+                                                <i class="bx bx-upload d-block d-sm-none"></i>
+                                                <input type="hidden" name="oldImage" value="{{ old('image') }}">
+                                                <input type="file" id="upload"
+                                                    class="account-file-input @error('image') is-invalid @enderror"
+                                                    name="image" hidden onchange="previewImageUmkm()">
+                                                @error('image')
+                                                    <div class="invalid-feedback text-light">
+                                                        {{ $message }}
+                                                    </div>
+                                                @enderror
+                                            </label>
+                                            <p class="text-muted mb-0">Allowed JPG, GIF or PNG. Max size of 800K</p>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                {{-- tambahan --}}
+                                <div class="row mb-3">
+                                    <label class="col-sm-2 col-form-label" for="description">Izin lain</label>
+                                    <div class="col-sm-10">
+                                        <div class="input-group input-group-merge flex-column">
+                                            <input type="hidden" name="description" id="description"
+                                                class="form-control phone-mask @error('description') is-invalid @enderror"
+                                                placeholder="Tulis disini izin lain yang anda butuhkan"
+                                                aria-describedby="basic-icon-default-address"
+                                                value="{{ old('description') }}" required>
+                                            <trix-editor input="description"></trix-editor>
+                                            @error('description')
+                                                <div class="invalid-feedback">
+                                                    {{ $message }}
+                                                </div>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div class="row justify-content-end">
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary"><span
+                                                class='bx bx-mail-send'></span>
+                                            &nbsp;
+                                            Send</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-12 mb-5">
+        <h4 class="col-6 fw-bold py-3 mb-2"><span class="text-muted fw-light">Data/</span> Pengajuan Bantuan Dana</h4>
+        <div class="card">
+            <h5 class="card-header">Tabel Pengajuan Bantuan Dana</h5>
+            <form action="/dashboard/izin/surat" method="get" class="d-flex mx-4 mb-2">
+                <input class="form-control me-2" type="text" name="search" id="search" placeholder="Search"
+                    aria-label="Search" value="{{ request('search') }}">
+                <button class="btn btn-outline-primary" type="submit">Search</button>
+            </form>
+            <div class="table-responsive text-nowrap">
+                {{-- <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>NIB</th>
+                            <th>Nama</th>
+                            <th>Phone No</th>
+                            <th>Kategori Perizinan</th>
+                            <th>Izin lain</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-border-bottom-0">
+                        @if ($izins->count())
+                            @foreach ($izins as $izin)
+                                <tr>
+                                    <td>{{ $izin->id }}</td>
+                                    <td><strong>{{ $izin->nib }}</strong></td>
+                                    <td>{{ $izin->name }}</td>
+                                    <td>{{ $izin->phonenumber }}</td>
+                                    <td><span
+                                            class="badge bg-label-primary me-1"><strong>{{ $izin->category->category }}</strong></span>
+                                    </td>
+                                    <td>{!! $izin->description !!}</td>
+                                    <td>
+                                        @if ($izin->status == 0)
+                                            <div class="spinner-border spinner-border-sm text-warning" role="status">
+                                                <span class="visually-hidden">Loading...</span>
+                                            </div><span class="ms-2 badge bg-label-warning me-1">Pending</span>
+                                        @elseif ($izin->status == 1)
+                                            <span class="text-info"><strong><i
+                                                        class="bx bx-check me-1"></i></strong></span>
+                                            <span class="badge bg-label-info me-1">Aproved</span>
+                                        @else
+                                            <span class="text-danger"><strong>
+                                                    <i class="bx bx-x me-1"></i></strong></span>
+                                            <span class="badge bg-label-danger me-1">Ditolak</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        <div class="dropdown">
+                                            <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
+                                                data-bs-toggle="dropdown">
+                                                <i class="bx bx-dots-vertical-rounded"></i>
+                                            </button>
+                                            <div class="dropdown-menu">
+                                                <form id="userDelete-form"
+                                                    action="/dashboard/izin/surat/{{ $izin->id }}"
+                                                    method="post">
+                                                    @method('delete')
+                                                    @csrf
+                                                    <button class="dropdown-item"
+                                                        onclick="return confirm('Apa anda yakin?')">
+                                                        <i class="bx bx-x me-1"></i> Batalkan
+                                                    </button>
+                                                </form>
+                                                <a class="dropdown-item" target="_blank"
+                                                    @if ($izin->file) @else
+                                            disabled @endif
+                                                    href="{{ asset('storage/' . $izin->file) }}" download><i
+                                                        class='bx bx-down-arrow-circle'></i>
+                                                    Download</a>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        @else
+                            <h1 class="text-center mt-5 mb-5">Izin not found :)</h1>
+                        @endif
+                    </tbody>
+                </table> --}}
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/dashboard/pages/dana/index.blade.php
+++ resources/views/dashboard/pages/dana/index.blade.php
@@ -5,7 +5,9 @@
     <div class="content-wrapper ">
         <!-- Content -->
         <div class="container-xxl flex-grow-1 container-p-y">
-            <h1>INI DANA</h1>
+            <div id="dana">
+                @include('dashboard.pages.dana.dana')
+            </div>
         </div>
 
         {{-- footer --}}
