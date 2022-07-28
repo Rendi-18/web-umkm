bb3576a358be2a0b0be13fd9237640fe29819e09
Rendi-18 <rendidwipratama99@gmail.com>
2022-07-28 19:34:44 +0700

    relasi

--- app/Models/Bantuan.php
+++ app/Models/Bantuan.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Bantuan extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function umkm()
+    {
+        return $this->belongsTo(Umkm::class, 'umkm_id');
+    }
 }
--- app/Models/Umkm.php
+++ app/Models/Umkm.php
@@ -25,4 +25,9 @@ class Umkm extends Model
     {
         return $this->hasMany(Product::class, 'umkm_id');
     }
+
+    public function bantuan()
+    {
+        return $this->hasMany(Umkm::class, 'umkm_id');
+    }
 }
